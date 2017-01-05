e7740ab946cc8d0bc242b6921face60a200125c1
Jaesup Kwak <veshboo@gmail.com>
2017-01-05 11:00:29 +0900

    bump version to upload pypi

--- redisrwlock/__init__.py
+++ redisrwlock/__init__.py
@@ -1,7 +1,7 @@
 import logging
 from .redisrwlock import _cmp_time, Rwlock, RwlockClient
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 # Set default logging handler to avoid "No handler found" warnings.
 try:  # Python 2.7+
